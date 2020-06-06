43099290c86a766bafbb0d058a4198c78bd81504
Shahab Jahangiri <shahab.j3@gmail.com>
2020-06-06 10:31:11 +1000

    added has many to company table and changed the logo

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,5 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :handyman
+  has_many :reviews
 end
--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,4 @@
 class Company < ApplicationRecord
   belongs_to :owner
+  has_many :handymen
 end
