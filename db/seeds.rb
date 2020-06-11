b0c8adba56d47ef316bda319c223dac70476bc3d
Shahab Jahangiri <shahab.j3@gmail.com>
2020-06-11 20:55:34 +1000

    seeded our file, application tested in rails console and assossiations working

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# create 5 users
+Handyman.destroy_all
+Company.destroy_all
+User.destroy_all
+shahab = User.create!(first_name: 'Shahab', last_name: 'Jahangiri',email: 'shahab.j3@gmail.com', password: '1234567')
+nicho = User.create!(first_name: 'Nicho', last_name: 'Proto',email: 'nicho@gmail.com', password: '1234567')
+daniel = User.create!(first_name: 'Daniel', last_name: 'Proto',email: 'daniel@gmail.com', password: '1234567')
+# create categories
+Category.destroy_all
+kitchen_install = Category.create!(title: "Kitchen Installation Services")
+painting = Category.create!(title: "Painting Services")
+roofing = Category.create!(title: "Roofing Services")
+artwork =  Category.create!(title: "Art work Services")
+# create companies is one of the users you
+
+handymanguys = Company.create!(name: "HandymanGuys Pty Ltd", abn: "54 634 046 138", description: "Handyman Services in Beautiful Melbourne", owner: shahab)
+artbybrush = Company.create!(name: "Art By Brush Pty Ltd", abn: "54 645 067 138", description: "Beautiful arts for sale", owner: daniel)
+# create handyman for companies each handyman belogs to a company and category
+ben = Handyman.create!(name: "Ben Jahan", description: "Handyman Services", company: shahab, category: kitchen_install)
+daniel = Handyman.create!(name: "Daniel ", description: "Artist", company: artbybrush, category: artwork)
