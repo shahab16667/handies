9361c210839d3f7a4a3ae864185f833266dd20bc
Shahab Jahangiri <shahab.j3@gmail.com>
2020-06-11 21:13:17 +1000

    seeded our file, application tested in rails console and assossiations working

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,17 +11,22 @@ Company.destroy_all
 User.destroy_all
 shahab = User.create!(first_name: 'Shahab', last_name: 'Jahangiri',email: 'shahab.j3@gmail.com', password: '1234567')
 nicho = User.create!(first_name: 'Nicho', last_name: 'Proto',email: 'nicho@gmail.com', password: '1234567')
+sy = User.create!(first_name: 'Sy', last_name: 'Rashid',email: 'sy@gmail.com', password: '1234567')
 daniel = User.create!(first_name: 'Daniel', last_name: 'Proto',email: 'daniel@gmail.com', password: '1234567')
+henry = User.create!(first_name: 'Henry', last_name: 'Lay',email: 'henry@gmail.com', password: '1234567')
 # create categories
 Category.destroy_all
 kitchen_install = Category.create!(title: "Kitchen Installation Services")
 painting = Category.create!(title: "Painting Services")
 roofing = Category.create!(title: "Roofing Services")
 artwork =  Category.create!(title: "Art work Services")
+dj =  Category.create!(title: "find Dj near Services")
 # create companies is one of the users you
 
 handymanguys = Company.create!(name: "HandymanGuys Pty Ltd", abn: "54 634 046 138", description: "Handyman Services in Beautiful Melbourne", owner: shahab)
 artbybrush = Company.create!(name: "Art By Brush Pty Ltd", abn: "54 645 067 138", description: "Beautiful arts for sale", owner: daniel)
+herodjs = Company.create!(name: "Hero Djs Pty Ltd", abn: "54 645 045 788", description: "find nearest Dj ", owner: henry)
 # create handyman for companies each handyman belogs to a company and category
 ben = Handyman.create!(name: "Ben Jahan", description: "Handyman Services", company: shahab, category: kitchen_install)
 daniel = Handyman.create!(name: "Daniel ", description: "Artist", company: artbybrush, category: artwork)
+henry = Handyman.create!(name: "Henry ", description: "Dj finder", company: artbybrush, category: artwork)
